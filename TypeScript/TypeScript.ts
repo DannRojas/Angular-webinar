f32beed7706ef6b2e46043a52dff970c012b6c5f
DannRojas <rojasc.dan@gmail.com>
2020-10-08 11:42:41 -0400

    Transpilate JS to TS

--- TypeScript/TypeScript.ts
+++ TypeScript/TypeScript.ts
@@ -1,14 +1,19 @@
 // Uso de Let y Const
-var nombre = "Ricardo Tapia";
-var edad = 23;
-var PERSONAJE = {
+let nombre:string = "Ricardo Tapia"; 
+let edad:number = 23;
+const PERSONAJE = {
   nombre: nombre,
   edad: edad
 };
 
 
 // Cree una interfaz que sirva para validar el siguiente objeto
-var batman = {
+interface SuperHero{
+  nombre?:string;
+  artesMarciales?:string[]
+}
+
+let batman:SuperHero = {
   nombre: "Bruno Díaz",
   artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
 }
@@ -24,12 +29,14 @@ function resultadoDoble( a, b ){
 // donde NOMBRE = obligatorio
 //       PODER  = opcional
 //       ARMA   = por defecto = "arco"
-function getAvenger( nombre, poder, arma ){
-  var mensaje;
+function getAvenger( nombre:string, poder?:string, arma:string = "arco" ){
+  let mensaje:string;
   if( poder ){
      mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+     mensaje = `${nombre} tiene el poder de: ${poder} y un arma: ${arma}`;
   }else{
-     mensaje = nombre + " tiene un " + poder
+     mensaje = nombre + " tiene un " + arma;
+     mensaje = `${nombre} tinen un ${arma}`;
   }
 };
 
@@ -41,3 +48,27 @@ function getAvenger( nombre, poder, arma ){
 //   * altura
 // También un método que calcule el área  =  base * altura,
 // ese método debe de retornar un numero.
+
+class rectangulo{
+  base: number;
+  altura: number;
+
+  areaF(): any{
+    let area: number = this.base * this.altura;
+    let prueba: string = "Hola mundo";
+    return prueba;
+  }
+}
+
+let figura:rectangulo = new rectangulo();
+figura.base = 10;
+figura.altura = 5;
+
+function factorial(x:number){
+  if(x===0){
+    return 1;
+  }else{
+    return x*factorial(x-1);
+  }
+}
+
